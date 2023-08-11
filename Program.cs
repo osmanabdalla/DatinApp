fbd38bafa5718147070dffcaf20905db46b270c0
osman abdalla <osmanabdalla1970@gmail.com>
2023-08-11 12:54:46 -0400

    end of section 3

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -16,7 +17,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
